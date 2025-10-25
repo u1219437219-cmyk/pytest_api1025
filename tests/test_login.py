da3e12a30cd4b2467c7cce9618de70662ac9c45f
u1219437219-cmyk <u1219437219@gmail.com>
2025-10-25 16:32:55 +0800

    新增登录测试用例：测试错误用户名场景

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,7 +3,8 @@ from utils.api_request import send_post
 
 @pytest.mark.parametrize("case", [
     {"username": "admin", "password": "123456", "expected_code": 200},
-    {"username": "admin", "password": "000000", "expected_code": 401}
+    {"username": "admin", "password": "000000", "expected_code": 401},
+    {"username": "wronguser", "password": "123456", "expected_code": 401}
 ])
 def test_login(base_url, case):
     url = f"{base_url}/login"
